f7b2189859420d543d8630a9d9bffc8019985d6b
Esteban Mutis <smithmutis21@gmail.com>
2024-05-31 10:00:17 -0500

    feat: Add ModeToggle component to footer
    The code changes in this commit add the ModeToggle component to the footer. This component provides a toggle button for switching between light and dark modes in the application. It enhances the user experience by allowing users to customize the visual appearance of the website.
    
    Fixes #123

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,20 +2,19 @@ import React from 'react'
 
 import { cn } from '@/lib/utils'
 
+import { ModeToggle } from './ui/theme-toggle-button'
+
 export function Footer() {
   return (
     <footer
       className={cn(
-        '[grid-column:full-width]',
-        'md:[grid-column:breakout]',
-        '[grid-row:content-main-end]',
-        'text-center',
-        'grid',
-        'place-items-center',
-        'm-2'
+        'relative my-2 grid w-full place-items-center text-center [grid-column:full-width] [grid-row:content-main-end]  md:[grid-column:breakout]'
       )}
     >
       <p className='text-center'>Con ❤️ por Esteban Mutis</p>
+      <div className='absolute right-0'>
+        <ModeToggle />
+      </div>
     </footer>
   )
 }
